c3d1dc41fb7a26636d26d97b35c57e66e9f88bc2
Shintani Teppei <teppest1215@gmail.com>
2019-05-11 12:43:03 +0900

    add rack-lineprof

--- webapp/ruby/isuwitter.rb
+++ webapp/ruby/isuwitter.rb
@@ -7,11 +7,17 @@ require 'sinatra/json'
 require 'mysql2-cs-bind'
 require 'erubis'
 
+require 'rack-lineprof'
+require 'logger' # このrequire要らないかも
+
 require './redis_client'
+
+logger = Logger.new('/var/log/lineprof.log')
 
 module Isuwitter
   class WebApp < Sinatra::Base
     use Rack::Session::Cookie, key: 'isu_session', secret: 'kioicho'
+    use Rack::Lineprof, profile: 'app.rb', logger: logger
     set :public_folder, File.expand_path('../../public', __FILE__)
 
     PERPAGE = 50
